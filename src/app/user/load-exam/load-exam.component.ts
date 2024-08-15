a7f54ed3c6d4fe25231b5de2befd7d95960040ff
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2024-08-15 04:49:04 -0500

    feat: add functionality of loading requests by category

--- src/app/user/load-exam/load-exam.component.ts
+++ src/app/user/load-exam/load-exam.component.ts
@@ -2,17 +2,42 @@ import {Component, OnInit} from '@angular/core';
 import {Exam} from "../../pages/model/exam.model";
 import {ExamApiService} from "../../services/exam-api.service";
 import {CategoryApiService} from "../../services/category-api.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, RouterLink} from "@angular/router";
+import {
+  MatCard,
+  MatCardActions,
+  MatCardAvatar,
+  MatCardContent, MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle
+} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
+import {NgForOf, NgIf} from "@angular/common";
+import {Category} from "../../pages/model/category.model";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-load-exam',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardContent,
+    MatCardActions,
+    MatButton,
+    RouterLink,
+    NgIf,
+    NgForOf,
+    MatCardAvatar,
+    MatCardHeader
+  ],
   templateUrl: './load-exam.component.html',
   styleUrl: './load-exam.component.css'
 })
-export class LoadExamComponent implements OnInit{
+export class LoadExamComponent implements OnInit {
   exams: Exam[] = [];
+  categories: Category[] = [];
   catId: any;
 
   constructor(
@@ -25,13 +50,13 @@ export class LoadExamComponent implements OnInit{
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.catId = params['catId'];
-
-      if (this.catId == 0) {
+      if (this.catId==0) {
         console.log("Cargando todos los exámenes");
         this.examService.getAllExamActive().subscribe(
           (data) => {
             this.exams = data;
             console.log(this.exams);
+            this.loadCategories();
           },
           (error) => {
             console.log(error);
@@ -39,16 +64,36 @@ export class LoadExamComponent implements OnInit{
         );
       } else {
         console.log(`Cargando exámenes de la categoría ${this.catId}`);
-        this.examService.getExamsByCategory(this.catId).subscribe(
+        this.examService.getExamsByCategoryAndActive(this.catId).subscribe(
           (data) => {
             this.exams = data;
             console.log(this.exams);
+            this.loadCategories();
           },
           (error) => {
             console.log(error);
           }
         );
       }
+    });
+  }
+
+  loadCategories(): void {
+    this.categoryService.getAllCategories().subscribe(
+      (categories) => {
+        this.categories = categories;
+        this.assignCategoriesToExams();
+      },
+      (error) => {
+        console.log(error);
+        Swal.fire('Error', 'Error al cargar las categorías', 'error');
+      }
+    );
+  }
+
+  assignCategoriesToExams(): void {
+    this.exams.forEach(exam => {
+      exam.category = this.categories.find(category => category.id === exam.categoryId);
     });
   }
 }
